627c1475a773675e670e6bb6f2539b99f640b117
Mateo Amaya Higuita <mateooamaya65@gmail.com>
2023-01-21 15:32:29 -0500

    first commit

---
+++ proyect/src/components/ButtonBox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ButtonBox = ({ colorRandom, handleClick }) => {
+  return (
+    <button
+      style={{ background: colorRandom }}
+      className="box__btn"
+      onClick={handleClick}>
+      &gt;
+    </button>
+  );
+};
+
+export default ButtonBox;
